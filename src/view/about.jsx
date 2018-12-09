55332b18f592e9c3c429252737908a8e9de2ed17
李梦克 <limengke@souche.com>
2018-12-09 17:55:30 +0800

    Revert "add word of page"
    This reverts commit 2c35294

--- src/view/about.jsx
+++ src/view/about.jsx
@@ -4,7 +4,7 @@ export default class About extends React.Component {
     render () {
         return (
             <div>
-                this is about page!
+                this is about
             </div>
         )
     }
